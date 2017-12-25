2439f7daed46791b4f83bc92fa86787ae2d0a595
duonnghan <duonnghan@gmail.com>
2017-12-25 07:21:24 +0700

    edit: do some unnecessary manipulation.

--- libs/config.php
+++ libs/config.php
@@ -8,22 +8,8 @@
     $dbUser = 'root';
     $dbPass = '';
     $dbName = 'qlsach';
+
     //So record hien thi tren trang
     $limit = 10;
-    
-    // tùy chỉnh web root và server root cho ứng dụng book shopping cart
-    //__FILE__: đường dẫn đến tập tin hiện tại
-    $thisFile = str_replace('\\', '/', __FILE__);//C:/xampp/htdocs/admincp/libs/config.php
-    $docRoot = $_SERVER['DOCUMENT_ROOT'];// C:/xampp/htdocs
-
-    $webRoot  = str_replace(array($docRoot, 'libs/config.php'), '', $thisFile);//   /admincp/
-    $srvRoot  = str_replace('libs/config.php', '', $thisFile);//    C:/xampp/htdocs/admincp/
-
-    define('WEB_ROOT', 'localhost:8080'.$webRoot);//    localhost:8080//admincp/
-    define('SRV_ROOT', $srvRoot);// C:/xampp/htdocs/admincp/
-
-    // các thư mục mà bạn muốn chứa tất cả thư mục và ảnh sản phẩm
-    define('CATEGORY_IMAGE_DIR', 'images/category/');
-    define('PRODUCT_IMAGE_DIR',  'images/product/');
-
+        
 ?>
